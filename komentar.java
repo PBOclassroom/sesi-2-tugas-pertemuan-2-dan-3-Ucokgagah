7cbdca0e730c5067db1119ce3ed7af7dbeb151cb
Rudi Kurnia Al Amin <118322273+Ucokgagah@users.noreply.github.com>
2023-09-21 11:06:33 +0700

    Tugas P 2 Dan 3 Rudi Kurnia Al Amin (22346041)

---
+++ komentar.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pertemuan;
+
+/**
+ *
+ * @author rudika65
+ */
+public class komentar {
+    public static void main(String args[]) {
+        String nama = "Rudi Kurnia al amin";
+        /*Nama lengkap */
+        String panggilan = "Rudi";
+        /*Nama Panggilan */
+        String anak_ke = "ke-4 dari 3 bersaudara";
+        /*Jumlah saudara */
+        String tanggal_lahir = "09 Mei 2003";
+        /*Tanggal lahir */
+        String tinggi_badan = "152 cm";
+        /*Tinggi badan*/
+        String berat_badan = "55 kg";
+        /*Berat badan */
+        String status = "Jomblo";
+        /*Status */
+        String prodi = "Informatika";
+        /*Jurusan Kuliah */
+        String cita_cita = "Pengusaha";
+        /*cita - cita saya */
+        String kata_kata_hari_ini = "Kerja keras tidak akan menghianati hasil";
+        /*Hanya sebuah kalimat unntuk memotivasi */
+        
+        System.out.println(nama);
+        System.out.println(panggilan);
+        System.out.println(anak_ke);
+        System.out.println(tanggal_lahir);
+        System.out.println(tinggi_badan);
+        System.out.println(berat_badan);
+        System.out.println(status);
+        System.out.println(prodi);
+        System.out.println(cita_cita);
+        System.out.println(kata_kata_hari_ini);
+        
+        
+    
+}
+    
+}
---
+++ latihan2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan3;
+
+/**
+ *
+ * @author asus
+ */
+public class latihan2 {
+public static void main(String[] args) {
+        // TODO code application logic here 
+        int mynumber = 88;
+        short myshort = 847;
+        long mylong = 9797;
+        double mydouble = 72.97;
+        float myfloat = 324.3f;
+        char mychar = 'y';
+        boolean myboolean = false;
+        byte mybyte = 127;
+        System.out.println(mynumber);
+        System.out.println(myshort);
+        System.out.println(mylong);
+        System.out.println(mydouble);
+        System.out.println(myfloat);
+        System.out.println(mychar);
+        System.out.println(myboolean);
+        System.out.println(mybyte);
+}
+}
